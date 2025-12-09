fda8db124c4fc985da9d902bf0170aa9b1a7352d
josh.fn7 <josh.fn7@kakaomobility.com>
2025-12-09 16:21:10 +0900

    [lib] Update: ComposableEffect
    - satisfies the StateMachine requirements

--- Sources/StateMachine/ComposableEffect.swift
+++ Sources/StateMachine/ComposableEffect.swift
@@ -30,12 +30,36 @@ Action.IOResult == IOResult,
 IOEffect : ComposableEffectConvertible
 {
     
+    static func reduce(_ state: State, _ action: Action) -> Transition {
+        return switch Action.map(action) {
+        case nil: identity
+        case .input(let input)?: reduceInput(state, input)
+        case .ioResult(let ioResult)?: reduceIOResult(state, ioResult)
+        }
+    }
+    
+    func applyIOEffect(_ ioEffect: IOEffect) -> Effect<Action> {
+        .run { send in
+            for try await result in runIOEffect(ioEffect) {
+                await send(.ioResult(result))
+            }
+        }
+    }
+    
     func apply(_ transition: Transition, to state: inout State) -> Effect<Action> {
         let (nextState, ioEffect) = transition
         if let nextState { state = nextState }
         return ioEffect.map { $0.asComposableEffect().extract(applyIOEffect(_:)) } ?? .none
     }
     
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            let transition = Self.reduce(state, action)
+            return apply(transition, to: &state)
+        }
+    }
+    
+
 }
 
 public extension ComposableEffect {
