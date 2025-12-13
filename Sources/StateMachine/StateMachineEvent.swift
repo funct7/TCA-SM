b6eb1fb4b520a8d16210e782a9919f94339fffea
Josh Park <funct7.io@gmail.com>
2025-12-13 12:44:37 +0900

    [lib] Update: StateMachineEvent
    - CasePathable & CasePathIterable

--- Sources/StateMachine/StateMachineEvent.swift
+++ Sources/StateMachine/StateMachineEvent.swift
@@ -1,5 +1,7 @@
 import Foundation
+import ComposableArchitecture
 
+@dynamicMemberLookup
 public enum StateMachineEvent<Input, IOResult> {
     case input(Input)
     case ioResult(IOResult)
@@ -12,3 +14,45 @@ extension StateMachineEvent : StateMachineEventConvertible {
     public static func map(_ action: Self) -> StateMachineEvent<Input, IOResult>? { action }
     
 }
+
+public extension StateMachineEvent {
+    
+    struct AllCasePaths: CasePaths.CasePathReflectable, Sendable, Sequence {
+        
+        public subscript(
+            root: StateMachineEvent<Input, IOResult>
+        ) -> CasePaths.PartialCaseKeyPath<StateMachineEvent<Input, IOResult>> {
+            if root.is(\.input) { return \.input }
+            if root.is(\.ioResult) { return \.ioResult }
+            return \.never
+        }
+        
+        public var input: CasePaths.AnyCasePath<StateMachineEvent<Input, IOResult>, Input> {
+            ._$embed(StateMachineEvent.input) {
+                guard case let .input(v0) = $0 else { return nil }
+                return v0
+            }
+        }
+        
+        public var ioResult: CasePaths.AnyCasePath<StateMachineEvent<Input, IOResult>, IOResult> {
+            ._$embed(StateMachineEvent.ioResult) {
+                guard case let .ioResult(v0) = $0 else { return nil }
+                return v0
+            }
+        }
+        
+        public func makeIterator(
+        ) -> Swift.IndexingIterator<[CasePaths.PartialCaseKeyPath<StateMachineEvent<Input, IOResult>>]> {
+            var allCasePaths: [CasePaths.PartialCaseKeyPath<StateMachineEvent<Input, IOResult>>] = []
+            allCasePaths.append(\.input)
+            allCasePaths.append(\.ioResult)
+            return allCasePaths.makeIterator()
+        }
+        
+    }
+    
+    static var allCasePaths: AllCasePaths { AllCasePaths() }
+    
+}
+
+extension StateMachineEvent: CasePaths.CasePathable, CasePaths.CasePathIterable { }
